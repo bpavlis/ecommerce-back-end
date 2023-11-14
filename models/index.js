c3078a0c2e7535f577720664958fa9f35db84b88
bpavlis <benpavlis@gmail.com>
2023-11-14 17:12:34 -0600

    did models for each file + index

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category ---- one to many?
+// Products belongsTo Category
+Product.belongsTo (Category, {
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products ---- many to many?
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+})
 
 module.exports = {
   Product,
